48a1ef0352d15fb36de4a5e15291cad5e6c020bc
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-17 23:27:11 -0300

    Generates 151 pokemon pages with getStaticProps and getStaticPaths, using dynamic routes

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,2 @@
+export * from './pokemon_detail'
 export * from './pokemon_list'
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,16 +1,42 @@
-import { useRouter } from 'next/router'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 
 import { Layout } from '../../components/layouts'
+import { pokeApi } from '../../api'
+import { Pokemon } from '../../interfaces'
 
-export const PokemonPage = () => {
-  const router = useRouter()
-  const { id } = router.query
+interface Props {
+  pokemon: Pokemon
+}
 
+const PokemonPage: NextPage<Props> = ({ pokemon }) => {
   return (
     <Layout title='Some pokemon'>
-      <div>PokemonPage - id: {id}</div>
+      <div>{ pokemon.name }</div>
     </Layout>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemons151 = [...Array(151)].map((_value, index) => `${index + 1}`)
+
+  return {
+    paths: pokemons151.map(id => ({
+      params: { id }
+    })),
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params as { id: string }
+
+  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${id}`)
+
+  return {
+    props: {
+      pokemon: data
+    }
+  }
 }
 
 export default PokemonPage
