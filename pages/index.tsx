cb5ecdb38794195ec095a1a93c37f3557e68cdb9
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-17 22:33:46 -0300

    Adds static props to index page - list of pokemons' raw data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,44 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
-import { Layout } from '../components/layouts'
+import { NextPage, GetStaticProps } from 'next'
 
-const Home: NextPage = () => {
+import { pokeApi } from '../api'
+import { Layout } from '../components/layouts'
+import { PokemonListResponse, SmallPokemon } from '../interfaces'
+
+interface Props {
+  pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Pokemons list'>
-      <Button color='gradient'>Hello World</Button>
+      <ul>
+        {
+          pokemons.map(({ id, name }) =>
+            <li key={id}>#{id} - {name}</li>
+          )
+        }
+      </ul>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => {
+    const id = i + 1
+    return {
+      ...pokemon,
+      id,
+      img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
+    }
+  })
+
+  return {
+    props: {
+      pokemons
+    }
+  }
 }
 
 export default Home
